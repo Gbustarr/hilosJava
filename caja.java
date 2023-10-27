6f0fd8c771beb88efa70bb0ac9ba05534e67a0dd
Yanko Acuña <yacuna20@alumnos.utalca.cl>
2023-10-27 20:57:44 -0300

    Implementacion de clase caja, para ejecutar los hilos.Instancia de controladores de eventos en el main

--- caja.java
+++ caja.java
@@ -1,9 +1,28 @@
 // caja puede ser tanto presencial como web
 public class caja extends Thread{
-    
+    evento_controller evento;
+    int idHilo;
+    int tipo;
+    String nombreHilo;
+
+    public caja(int idHilo, int tipo){
+        this.idHilo = idHilo;
+        this.tipo = tipo;
+        if(tipo == 0){
+            nombreHilo = "Caja " + idHilo;
+        }
+        else{
+            nombreHilo = "Hilo Web " + idHilo;
+        }
+    }
+
+    public void setEvento(evento_controller evento){
+        this.evento = evento;
+    }
 
     @Override
     public void run() {
-        
+
+        evento.comprar(tipo,nombreHilo);
     }
 }
--- puntoEventos.java
+++ puntoEventos.java
@@ -1,10 +1,11 @@
+import java.io.File;
 import java.util.ArrayList;
 
 class puntoEventos {
     public static void main(String[] args) {
 
         ArrayList<info_evento> eventos = new ArrayList<info_evento>();
-
+        
         reader r = new reader();
         // Lectura de archivo eventos.txt
         r.leer();
@@ -13,6 +14,16 @@ class puntoEventos {
 
         //Guardando el arraylist generado por el reader
         eventos = r.getEventos();
+        ArrayList<evento_controller> controladorEventos = new ArrayList<>();
+        for(int i=0; i<eventos.size(); i++){
+            evento_controller e = new evento_controller(i+"", eventos.get(i), new File("27-10.txt"));
+            controladorEventos.add(e);
+        }
+
+        //Hilos caja presencial
+        caja caja1 = new caja(1, 0);
+        caja1.setEvento(controladorEventos.get(0));
+        caja1.start();
 
         
 
